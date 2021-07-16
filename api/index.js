63eef4ff17ecdb28f31d8704a31b95f91ca48de6
maulana4444 <84928555+maulana4444@users.noreply.github.com>
2021-07-17 05:45:28 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         msg.chat.id,
         `hello ${msg.chat.first_name}, welcome...\n
-        click /menu to main menu`
+        click /predict`
     );   
 });
 
@@ -24,16 +24,51 @@ bot.onText(/\/menu/, (msg) => {
     console.log(msg)
     bot.sendMessage(
         msg.chat.id,
-        `this is your main menu`
-    );   
+        `masukan nilai x1|x2|x3 contoh 9|9|9`
+    );
+    state = 1;
 });
+
+
+bot.on('message', (msg) =? {
+    if(state ==1){
+        s = msg.text.split("|");
+        x1 = s[0]
+        x2 = s[1]
+        x3 = s[3]
+        model.predict(
+            [
+                parseFloat(s[0]), //string to float
+                parseFloat(s[1]),
+                parseFloat(s[2]),
+            ]
+            
+        ).then((jres)=>{
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai x1 yang di prediksi adalah ${jres[0]}`
+            );
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai x2 yang di prediksi adalah ${jres[1]}`
+            );
+            bot.sendMessage(
+                msg.chat.id,
+                `nilai x3 yang di prediksi adalah ${jres[2]}`
+            );
+        })
+    }else{
+    state = 0
+}
+})
 
 // routers
 r.get('/prediction/:i/:r', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.i), // string to float
-            parseFloat(req.params.r)
+            parseFloat(req.params.x1), // string to float
+            parseFloat(req.params.x2)
+            parseFloat(req.params.x3)
         ]
     ).then((jres)=>{
         res.json(jres);
